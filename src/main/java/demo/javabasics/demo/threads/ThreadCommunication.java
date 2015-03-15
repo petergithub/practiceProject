63a0bdafbef55a210d68bb37c99b8db800715b93
peter <samplainsboro@gmail.com>
2015-03-15 18:46:09 +0800

    写两个线程，一个线程打印 1到52，另一个线程打印字母A到Z。打印顺序为12A34B56C……5152Z。要求用线程间的通信

---
+++ src/main/java/demo/javabasics/demo/threads/ThreadCommunication.java
@@ -0,0 +1,79 @@
+package demo.javabasics.demo.threads;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 写两个线程，一个线程打印 1到52，另一个线程打印字母A到Z。打印顺序为12A34B56C……5152Z。要求用线程间的通信。
+ * http://blog.csdn.net/shenbau/article/details/28093379
+ * 
+ * @version Date: Mar 15, 2015 5:01:25 PM
+ * @author Shang Pu
+ */
+public class ThreadCommunication {
+	private static final Logger log = LoggerFactory.getLogger(ThreadCommunication.class);
+
+	@Test
+	public void testThreadCommunication() {
+		Object obj = new Object();
+		NumberPrinter numberPrinter = new NumberPrinter(obj);
+		LetterPrinter letterPrinter = new LetterPrinter(obj);
+		numberPrinter.start();
+		letterPrinter.start();
+	}
+
+	public void testPrintChar() {
+		for (int i = 65; i <= 90; i++) {
+			log.info("i = {}", (char) i);
+		}
+		for (char i = 'A'; i <= 'Z'; i++) {
+			System.out.print(i);
+		}
+	}
+	
+	class NumberPrinter extends Thread {
+		private Object obj;
+		public NumberPrinter(Object obj) {
+			this.obj = obj;
+		}
+		public void run() {
+			synchronized (obj) {
+				for (int i = 1; i <= 52; i++) {
+					System.out.print(i);
+					if (i % 2 == 0) {
+						obj.notifyAll();
+						try {
+							obj.wait();
+						} catch (InterruptedException e) {
+							log.error("InterruptedException in .run()", e);
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	class LetterPrinter extends Thread {
+		private Object obj;
+		public LetterPrinter(Object obj) {
+			this.obj = obj;
+		}
+		public void run() {
+			synchronized (obj) {
+				for (char i = 'A'; i <= 'Z'; i++) {
+					System.out.print(i);
+					obj.notifyAll();
+					try {
+						if (i != 'Z') {// 最后一个就不要等了
+							obj.wait();
+						}
+					} catch (InterruptedException e) {
+						log.error("InterruptedException in .run()", e);
+					}
+				}
+			}
+		}
+	}
+
+}
