a0c2ea2acdcf51fca8b78b48716c91a0250b17da
peter <samplainsboro@gmail.com>
2016-09-26 15:16:35 +0800

    Download file from URL

--- src/main/java/org/pu/utils/IoUtils.java
+++ src/main/java/org/pu/utils/IoUtils.java
@@ -7,6 +7,7 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
 import java.io.Closeable;
 import java.io.File;
 import java.io.FileInputStream;
@@ -24,6 +25,7 @@ import java.io.PrintWriter;
 import java.io.RandomAccessFile;
 import java.io.Reader;
 import java.io.Writer;
+import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -78,6 +80,65 @@ public class IoUtils {
 			close(br);
 		}
 		return list;
+	}
+
+	/**
+	 * 从网络URL下载文件
+	 * 
+	 * @param urlStr
+	 * @param fileName
+	 * @param savePath
+	 * @throws IOException
+	 */
+	public static void downLoadFromUrl(String urlStr, String fileName, String savePath)
+			throws IOException {
+		log.debug("urlStr: {} fileName: {} savePath: {}", urlStr, fileName, savePath);
+		URL url = new URL(urlStr);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		// 设置超时间为3秒
+		conn.setConnectTimeout(3 * 1000);
+		// 防止屏蔽程序抓取而返回403错误
+		conn.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+
+		// 得到输入流
+		InputStream inputStream = conn.getInputStream();
+		// 获取自己数组
+		byte[] getData = readInputStream(inputStream);
+
+		// 文件保存位置
+		File saveDir = new File(savePath);
+		if (!saveDir.exists()) {
+			saveDir.mkdir();
+		}
+		File file = new File(saveDir + File.separator + fileName);
+		FileOutputStream fos = new FileOutputStream(file);
+		fos.write(getData);
+		if (fos != null) {
+			fos.close();
+		}
+		if (inputStream != null) {
+			inputStream.close();
+		}
+
+		log.debug("info: {} download save as {}/{}", url, savePath, fileName);
+	}
+
+	/**
+	 * 从输入流中获取字节数组
+	 * 
+	 * @param inputStream
+	 * @return
+	 * @throws IOException
+	 */
+	public static byte[] readInputStream(InputStream inputStream) throws IOException {
+		byte[] buffer = new byte[1024];
+		int len = 0;
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		while ((len = inputStream.read(buffer)) != -1) {
+			bos.write(buffer, 0, len);
+		}
+		bos.close();
+		return bos.toByteArray();
 	}
 
 	/**
@@ -1067,8 +1128,7 @@ public class IoUtils {
 		if (closeable != null) {
 			try {
 				closeable.close();
-			} catch (IOException e) {
-				// ignore
+			} catch (IOException ignore) {
 			}
 		}
 	}
