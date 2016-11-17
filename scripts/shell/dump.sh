59e3bff43cfeb1551c25a47d19c791bb7581146f
peter <samplainsboro@gmail.com>
2016-11-17 12:35:37 +0800

    dump shell update

--- scripts/shell/dump.sh
+++ scripts/shell/dump.sh
@@ -6,74 +6,93 @@
 #所以最好在kill脚本之前调用dump,进行自动备份,这样就不会有人为疏忽。
 
 JAVA_HOME=/usr/java
-OUTPUT_HOME=~/output
 DEPLOY_HOME=`dirname $0`
-HOST_NAME=`hostname`
 
-DUMP_PIDS=`ps  --no-heading -C java -f --width 1000 | grep "$DEPLOY_HOME" |awk '{print $2}'`
-if [ -z "$DUMP_PIDS" ]; then
-    echo "The server $HOST_NAME is not started!"
-    exit 1;
+PROJECT_NAME=$1
+DEPLOY_DIR=`pwd`
+LOGS_DIR=log
+if [ $# -lt 1 ]; then
+    echo "ERROR: Required one parameter! Usage: dump.sh PID/ProjectName"
+    exit 1
 fi
 
-DUMP_ROOT=$OUTPUT_HOME/dump
-if [ ! -d $DUMP_ROOT ]; then
-	mkdir $DUMP_ROOT
+if [ -z "$SERVER_NAME" ]; then
+    SERVER_NAME=`hostname`
 fi
 
-DUMP_DATE=`date +%Y%m%d%H%M%S`
-DUMP_DIR=$DUMP_ROOT/dump-$DUMP_DATE
+PIDS=`ps  --no-heading -C java -f --width 1000 | grep "$PROJECT_NAME" |awk '{print $2}'`
+
+echo -e "PIDS="$PIDS
+if [ -z "$PIDS" ]; then
+    echo "ERROR: The $PIDS does not started!"
+    exit 1
+fi
+
+if [ ! -d $LOGS_DIR ]; then
+    mkdir $LOGS_DIR
+fi
+DUMP_DIR=$LOGS_DIR/dump
 if [ ! -d $DUMP_DIR ]; then
-	mkdir $DUMP_DIR
+    mkdir $DUMP_DIR
+fi
+DUMP_DATE=`date +%Y%m%d%H%M%S`
+DATE_DIR=$DUMP_DIR/$DUMP_DATE
+if [ ! -d $DATE_DIR ]; then
+    mkdir $DATE_DIR
 fi
 
-echo -e "Dumping the server $HOST_NAME ...\c"
-for PID in $DUMP_PIDS ; do
-	$JAVA_HOME/bin/jstack $PID > $DUMP_DIR/jstack-$PID.dump 2>&1
-	echo -e ".\c"
-	$JAVA_HOME/bin/jinfo $PID > $DUMP_DIR/jinfo-$PID.dump 2>&1
-	echo -e ".\c"
-	$JAVA_HOME/bin/jstat -gcutil $PID > $DUMP_DIR/jstat-gcutil-$PID.dump 2>&1
-	echo -e ".\c"
-	$JAVA_HOME/bin/jstat -gccapacity $PID > $DUMP_DIR/jstat-gccapacity-$PID.dump 2>&1
-	echo -e ".\c"
-	$JAVA_HOME/bin/jmap $PID > $DUMP_DIR/jmap-$PID.dump 2>&1
-	echo -e ".\c"
-	$JAVA_HOME/bin/jmap -heap $PID > $DUMP_DIR/jmap-heap-$PID.dump 2>&1
-	echo -e ".\c"
-	$JAVA_HOME/bin/jmap -histo $PID > $DUMP_DIR/jmap-histo-$PID.dump 2>&1
-	echo -e ".\c"
-	if [ -r /usr/sbin/lsof ]; then
-	/usr/sbin/lsof -p $PID > $DUMP_DIR/lsof-$PID.dump
-	echo -e ".\c"
-	fi
+echo -e "Dumping the $SERVER_NAME ...\c"
+for PID in $PIDS ; do
+    jstack $PID > $DATE_DIR/jstack-$PID.dump 2>&1
+    echo -e ".\c"
+    jinfo $PID > $DATE_DIR/jinfo-$PID.dump 2>&1
+    echo -e ".\c"
+    jstat -gcutil $PID > $DATE_DIR/jstat-gcutil-$PID.dump 2>&1
+    echo -e ".\c"
+    jstat -gccapacity $PID > $DATE_DIR/jstat-gccapacity-$PID.dump 2>&1
+    echo -e ".\c"
+    jmap $PID > $DATE_DIR/jmap-$PID.dump 2>&1
+    echo -e ".\c"
+    jmap -heap $PID > $DATE_DIR/jmap-heap-$PID.dump 2>&1
+    echo -e ".\c"
+    jmap -histo $PID > $DATE_DIR/jmap-histo-$PID.dump 2>&1
+    echo -e ".\c"
+    jmap -dump:live,format=b,file=$DATE_DIR/jmap-dump-$PID.bin $PID 2>&1
+    echo -e ".\c"
+    if [ -r /usr/sbin/lsof ]; then
+    /usr/sbin/lsof -p $PID > $DATE_DIR/lsof-$PID.dump
+    echo -e ".\c"
+    fi
 done
-if [ -r /usr/bin/sar ]; then
-/usr/bin/sar > $DUMP_DIR/sar.dump
-echo -e ".\c"
-fi
-if [ -r /usr/bin/uptime ]; then
-/usr/bin/uptime > $DUMP_DIR/uptime.dump
-echo -e ".\c"
-fi
-if [ -r /usr/bin/free ]; then
-/usr/bin/free -t > $DUMP_DIR/free.dump
-echo -e ".\c"
-fi
-if [ -r /usr/bin/vmstat ]; then
-/usr/bin/vmstat > $DUMP_DIR/vmstat.dump
-echo -e ".\c"
-fi
-if [ -r /usr/bin/mpstat ]; then
-/usr/bin/mpstat > $DUMP_DIR/mpstat.dump
+if [ -r /bin/netstat ]; then
+/bin/netstat -an > $DATE_DIR/netstat.dump 2>&1
 echo -e ".\c"
 fi
 if [ -r /usr/bin/iostat ]; then
-/usr/bin/iostat > $DUMP_DIR/iostat.dump
+/usr/bin/iostat > $DATE_DIR/iostat.dump 2>&1
 echo -e ".\c"
 fi
-if [ -r /bin/netstat ]; then
-/bin/netstat > $DUMP_DIR/netstat.dump
+if [ -r /usr/bin/mpstat ]; then
+/usr/bin/mpstat > $DATE_DIR/mpstat.dump 2>&1
+echo -e ".\c"
+fi
+if [ -r /usr/bin/vmstat ]; then
+/usr/bin/vmstat > $DATE_DIR/vmstat.dump 2>&1
+echo -e ".\c"
+fi
+if [ -r /usr/bin/free ]; then
+/usr/bin/free -t > $DATE_DIR/free.dump 2>&1
+echo -e ".\c"
+fi
+if [ -r /usr/bin/sar ]; then
+/usr/bin/sar > $DATE_DIR/sar.dump 2>&1
+echo -e ".\c"
+fi
+if [ -r /usr/bin/uptime ]; then
+/usr/bin/uptime > $DATE_DIR/uptime.dump 2>&1
 echo -e ".\c"
 fi
 echo "OK!"
+echo "DUMP: $DATE_DIR"
+
+
