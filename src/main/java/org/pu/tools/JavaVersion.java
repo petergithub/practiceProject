bcc75d5f50d03f459985c0e38642969f4dfa2baf
peter <samplainsboro@gmail.com>
2017-08-27 12:38:38 +0800

    Java version print

---
+++ src/main/java/org/pu/tools/JavaVersion.java
@@ -0,0 +1,7 @@
+package org.pu.tools;
+public class JavaVersion {
+  public static void main(String[] args) {
+    System.out.println(System.getProperty("java.runtime.version"));
+    System.out.println(System.getProperty("java.home"));
+  }
+}
