5cbfc9e59a683f35f88005d43703e8864dd9b54f
peter <samplainsboro@gmail.com>
2018-08-18 12:53:19 +0800

    enum practice

--- src/main/java/demo/javabasics/demo/EnumDemo.java
+++ src/main/java/demo/javabasics/demo/EnumDemo.java
@@ -2,18 +2,25 @@ package demo.javabasics.demo;
 
 import java.util.EnumSet;
 
-public class EnumDemo {
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-	public enum Food {
+import junit.framework.Assert;
+
+public class EnumDemo {
+    private static final Logger log = LoggerFactory.getLogger(EnumDemo.class);
+
+	public enum FoodEnum {
 		HAMBURGER, FRIES, HOTDOG, ARTICHOKE;
-		public static Food favoriteFood() {
+		public static FoodEnum favoriteFood() {
 			return ARTICHOKE;
 		}
 	}
 
-	public enum Food2 {
+	public enum FoodPriceEnum {
 		HAMBURGER(7), FRIES(2), HOTDOG(3), ARTICHOKE(4);
-		Food2(int price) {
+		FoodPriceEnum(int price) {
 			this.price = price;
 		}
 
@@ -24,25 +31,49 @@ public class EnumDemo {
 		}
 	}
 
-	@org.junit.Test
+    public enum FoodCustomizeNameEnum {
+        HAMBURGER("Hamburger", 7), FRIES("Fries", 2), HOTDOG("Hotdog", 3), ARTICHOKE("Artichoke", 4);
+        
+        FoodCustomizeNameEnum(String alias, int price) {
+            this.price = price;
+        }
+
+        private final int price;
+
+        public int getPrice() {
+            return price;
+        }
+    }
+
+	
+	@Test
+	public void enumName() {
+	    Assert.assertEquals(FoodEnum.FRIES.name(), "FRIES");
+	    Assert.assertEquals(FoodPriceEnum.FRIES.name(), "FRIES");
+	    Assert.assertEquals(FoodCustomizeNameEnum.FRIES.name(), "Fries");
+	    Assert.assertEquals(FoodEnum.values().length, 4);
+	}
+	
+	@Test
 	public void testFood() {
-		for (Food f : Food.values()) {
+		for (FoodEnum f : FoodEnum.values()) {
 			System.out.println("Food: " + f);
-			if (f == Food.FRIES) {
+			if (f == FoodEnum.FRIES) {
 				System.out.println("I found the fries!");
 			}
-			if (f == Food.favoriteFood()) {
+			if (f == FoodEnum.favoriteFood()) {
 				System.out.println("I found my favorite food!");
 			}
 		}
 
-		for (Food f : EnumSet.range(Food.FRIES, Food.ARTICHOKE)) {
+		for (FoodEnum f : EnumSet.range(FoodEnum.FRIES, FoodEnum.ARTICHOKE)) {
 			System.out.println("More food: " + f);
 		}
 	}
 
+    @Test
 	public void testFood2() {
-		for (Food2 f : Food2.values()) {
+		for (FoodPriceEnum f : FoodPriceEnum.values()) {
 			System.out.print("Food2: " + f + ", ");
 
 			if (f.getPrice() >= 4) {
