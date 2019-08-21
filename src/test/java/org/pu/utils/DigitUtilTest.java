a650bf8b341f09b044fd12c38ad40ccf9ae842c0
peter <samplainsboro@gmail.com>
2019-08-21 13:54:24 +0800

    upgrade spring jpa version

---
+++ src/test/java/org/pu/utils/DigitUtilTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DigitUtilTest {
+
+    @Test
+    public void parseDigits() {
+    }
+
+    @Test
+    public void isDigits() {
+    }
+}
